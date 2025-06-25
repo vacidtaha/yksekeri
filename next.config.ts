202b36853a38bda723d17b926ea7f37f67a3626d
vacidtaha <tahavacid@icloud.com>
2025-06-26 01:15:44 +0300

    Fix Next.js Image component YouTube thumbnail error - Add external image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,46 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i1.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i2.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i3.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i4.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
